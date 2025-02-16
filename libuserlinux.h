2bc3fd2def307e6b0f5730d4db9d0efe06ae58e5
Eduardo Gerhardt <comercial.eduardogerhardt@gmail.com>
2025-02-15 21:06:32 -0300

    fix: fix header

--- libuserlinux.h
+++ libuserlinux.h
@@ -1,23 +1,28 @@
-/**
+/*
  *
- *  @title{libuserlinux: API to add or delete Linux users}
- * 	This code implements Linux user management functions.
- * 
- *  @brief   Linux User Management Support
- *  @file    libuserlinux.h
- *  @author  Eduardo Gerhardt (comercial.eduardogerhardt@gmail.com)
+ * Module: libuserlinux.h
+ * Description: API for managing Linux users.
+ *
+ * This header defines structures and function prototypes for adding,
+ * deleting, and modifying user accounts and passwords in a custom Linux
+ * environment.
+ *
+ * Author: Eduardo Gerhardt (comercial.eduardogerhardt@gmail.com)
  *
  */
 
-/* ****************
- * INCLUDED FILES *
- * ****************/
+#ifndef LIBUSERLINUX_H
+#define LIBUSERLINUX_H
+
+/*==============================================================================
+                          INCLUDES
+==============================================================================*/
 
 #include <stdbool.h>
- 
-/* *********************************
- * DEFINITION OF PUBLIC STRUCTURES *
- * *********************************/
+
+/*==============================================================================
+                          PUBLIC STRUCTURES
+==============================================================================*/
 
 typedef struct {
 	char *pszUserName;
@@ -27,33 +32,35 @@ typedef struct {
 	char *pszUserInfo;
 	char *pszUserDir;
 	bool bEnableEncryption;
-}stUserData;
+} stUserData;
 
-/* ********************************
- * PROTOTYPES OF PUBLIC FUNCTIONS *
- * ********************************/
+/*==============================================================================
+                         FUNCTION PROTOTYPES
+==============================================================================*/
 
 /**
- * @title{Linux User Manager - Add New User}
+ * Linux User Manager - Add New User
  *
  * This function check if a user exists in Linux.
- * @param[in] pszCheckUsername - Username to check.
+ *
+ * @param pszCheckUsername - Username to check.
  * @return Returns 0 in success.
  * @return Returns 22 (EINVAL) invalid argument.
  */
 int libuserlinux_checkuser(char *pszCheckUsername);
 
 /**
- * @title{Linux User Manager - Add New User}
+ * Linux User Manager - Add New User
  *
  * This function creates a new user in Linux.
- * @param[in] pszUserName - Username.
- * @param[in] pszUserPassword - User password.
- * @param[in] uiUserID - User ID.
- * @param[in] uiGroupID - Group ID.
- * @param[in] pszUserInfo - User info.
- * @param[in] pszUserDir - User directory.
- * @param[in] bEnableEncryption - Encrypt the password.
+ *
+ * @param] pszUserName - Username.
+ * @param] pszUserPassword - User password.
+ * @param] uiUserID - User ID.
+ * @param] uiGroupID - Group ID.
+ * @param] pszUserInfo - User info.
+ * @param] pszUserDir - User directory.
+ * @param] bEnableEncryption - Encrypt the password.
  * @return Returns 0 in success.
  * @return Returns 22 (EINVAL) invalid argument.
  * @return Returns 13 (EACCES) error accessing a file.
@@ -61,22 +68,25 @@ int libuserlinux_checkuser(char *pszCheckUsername);
 int libuserlinux_adduser(stUserData *pstUserData);
 
 /**
- * @title{Linux User Manager - Delete User}
+ * Linux User Manager - Delete User
  *
  * This function delete a user in Linux.
  */
 int libuserlinux_deluser(char *pszUsername);
 
 /**
- * @title{Linux User Manager - Change user password}
+ * Linux User Manager - Change user password
  *
  * This function change a user's password.
  */
 int libuserlinux_changepassword(char *pszUsername, char *pszNewPasswd);
 
+
 /**
- * @title{Linux User Manager - Check if user exists}
+ * Linux User Manager - Check if user exists
  *
  * This function check if user exists.
  */
 int libuserlinux_checkuser(char *pszUsername);
+
+#endif /* LIBUSERLINUX_H */
